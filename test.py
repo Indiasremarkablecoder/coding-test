9b27278bf49e8299c3720e6c4bd725dc31fb32a0
Indiasremarkablecoder <kinjal21dm@gmail.com>
2026-05-31 21:01:33 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+dict()
+{"name":"sidharth","score":76}
+{"name":"zoya","score":89}
+{"name":"naman","score":67}
+{"name":"parth","score":90}
+{"name":"lara","score":99}
+for i in range():
+    max.("score")
+    min.("score")
